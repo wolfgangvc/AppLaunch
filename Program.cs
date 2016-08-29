dffbdb9e45901fe4172547ca8a0c8f5751af835f
wolfgang <wolfgang>
2016-08-29 14:04:01 +0100

    Oh my God this program is stupid

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Diagnostics;
+
+namespace Applaunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Process.GetCurrentProcess().MainModule.FileName;
+            fileName = Path.GetFileNameWithoutExtension(fileName) + ".lnk";
+            if (File.Exists(fileName))
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = fileName;
+                proc.Start();
+            }
+        }
+    }
+}
